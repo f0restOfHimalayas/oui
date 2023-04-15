f050be462de0f565b377dc99ffedd30cdc1dbf57
Vishal Anand <anvishal@vmware.com>
2023-04-15 21:57:05 +0530

    Add Open API spec for version selection

---
+++ Makefile
@@ -0,0 +1,27 @@
+.PHONY : help clean build run pack all
+
+help:
+	echo "help: available targets: clean, build, run, pack, all"
+
+clean:
+	if [ -f open-api-gui ]; then
+		rm open-api-gui
+	fi
+
+build:
+	$(MAKE) clean
+	go build -ldflags="-s -w" -o open-api-gui main.go mime.go
+
+run:
+	go run -ldflags="-s -w" -o open-api-gui main.go mime.go
+
+pack:
+	$(MAKE) build
+	upx open-api-gui
+
+all:
+	GOOS=linux GOARCH=arm go build -o open-api-gui-linux-arm main.go mime.go
+	GOOS=linux GOARCH=arm64 go build -o open-api-gui-linux-arm64 main.go mime.go
+	GOOS=freebsd GOARCH=386 go build -o open-api-gui-breebsd-386 main.go mime.go
+	GOOS=windows GOARCH=amd64 go build -o open-api-gui-win-64 main.go mime.go
+	GOOS=windows GOARCH=arm64 go build -o open-api-gui-win-arm64 main.go mime.go
--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,47 +1,34 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import Root from './lib/Root.svelte'
+  let preview = "preview";
+  function callbackFunction(event) {
+      console.log(event)
+      preview = event.detail;
+  }
+
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Introduction</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+        </li>
+    </ul>
 
-  <div class="card">
-    <Counter />
-  </div>
+    <section class="card">
+        <Root on:notify="{callbackFunction}" />
+    </section>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+    <section class="preview card m-3">
+        {preview}
+    </section>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
